312c3776508aee64d884dbfcd3947ae242f4e260
harveyx22 <52708955+harveyx22@users.noreply.github.com>
2020-06-14 13:18:01 -0700

    Added Event Item Class and Event Item Context

--- EventCatalogAPI/Data/CatalogContext.cs
+++ EventCatalogAPI/Data/CatalogContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EventCatalogAPI.Domain;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,13 +9,66 @@ namespace EventCatalogAPI.Data
 {
     public class CatalogContext : DbContext
     {
-        public CatalogContext(DbContextOptions options): base(options)
+        public CatalogContext(DbContextOptions options) : base(options)
         {
 
         }
+        
+        public DbSet<EventItem> EventItems { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<EventItem>(i =>
+            {
+                i.ToTable("EventItems");
 
+                i.Property(p => p.Id)
+                    .IsRequired()
+                    .UseHiLo("event_item_hilo");
+
+                i.Property(p => p.Name)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                i.Property(p => p.Summary)
+                    .IsRequired()
+                    .HasMaxLength(1000);
+
+                i.Property(p => p.Description)
+                    .IsRequired();
+
+                i.Property(p => p.StreetAddress)
+                    .IsRequired();
+
+                i.Property(p => p.StartTime)
+                    .IsRequired();
+
+                i.Property(p => p.EndTime)
+                    .IsRequired();
+
+                i.Property(p => p.Price)
+                    .IsRequired();
+
+                i.Property(p => p.PictureURL)
+                    .IsRequired();
+
+                i.HasOne(c => c.EventOrganizer)
+                    .WithMany()
+                    .HasForeignKey(c => c.EventOrganizerId);
+
+                i.HasOne(c => c.EventType)
+                    .WithMany()
+                    .HasForeignKey(c => c.EventTypeId);
+
+                i.HasOne(c => c.EventState)
+                    .WithMany()
+                    .HasForeignKey(c => c.EventStateId);
+
+                i.HasOne(c => c.EventCounty)
+                    .WithMany()
+                    .HasForeignKey(c => c.EventCountyId);
+            });
         }
-    }
+    }    
 }
+
---
+++ EventCatalogAPI/Domain/EventItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventCatalogAPI.Domain
+{
+    public class EventItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Summary { get; set; }
+        public string Description { get; set; }
+        public string StreetAddress { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public decimal Price { get; set; }
+        public string PictureURL { get; set; }
+        public int EventOrganizerId { get; set; }
+        public int EventTypeId { get; set; }
+        public int EventStateId { get; set; }
+        public int EventCountyId { get; set; }
+
+        // Property types below may need to be changed if class names are different
+        public EventOrganizer EventOrganizer { get; set; }
+        public EventType EventType { get; set; }
+        public EventState EventState { get; set; }
+        public EventCounty EventCounty { get; set; }
+    }
+}
