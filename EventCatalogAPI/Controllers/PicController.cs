0d270a41d460fd46d21eff472b9d4104ea3af2b6
harveyx22 <52708955+harveyx22@users.noreply.github.com>
2020-06-26 20:28:08 -0700

    Added Pics, Pic Controller, Items Seeds, and Fixed Class Names

---
+++ EventCatalogAPI/Controllers/PicController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using EventCatalogAPI.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace ProductCatalogAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PicController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _env;
+        public PicController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult GetImage(int id)
+        {
+            var webRoot = _env.WebRootPath;
+            var path = Path.Combine($"{webRoot}/Pics/", $"Event{id}.jpg");
+            var buffer = System.IO.File.ReadAllBytes(path);
+
+            return File(buffer, "image/jpeg");
+        }
+    }
+}
