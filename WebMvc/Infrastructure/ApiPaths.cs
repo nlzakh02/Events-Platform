1c90123e940c514c45db01b0cd3f5d47ff31037a
harveyx22 <52708955+harveyx22@users.noreply.github.com>
2020-07-24 20:51:20 -0700

    Added WebMvc with Infrastructure (Including ApiPaths and CustomHttpClient)

---
+++ WebMvc/Infrastructure/ApiPaths.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMvc.Infrastructure
+{
+    public static class ApiPaths
+    {
+        public static class Catalog
+        {
+            public static string GetAllTypes(string baseUri)
+            {
+                return $"{baseUri}/EventTypes";
+            }
+
+            public static string GetAllOrganizers(string baseUri)
+            {
+                return $"{baseUri}/EventOrganizers";
+            }
+
+            public static string GetAllEventItems(string baseUri, int page, int take, int? type, int? organizer)
+            {
+                var filterQs = string.Empty;
+
+                if (type.HasValue || organizer.HasValue)
+                {
+                    var typeQs = (type.HasValue) ? type.Value.ToString() : " ";
+                    var organizerQs = (organizer.HasValue) ? organizer.Value.ToString() : " ";
+                    filterQs = $"/type/{typeQs}/organizer/{organizerQs}";
+                }
+
+                return $"{baseUri}items{filterQs}?pageIndex={page}&pageSize={take}";
+            }
+        }
+    }
+}
