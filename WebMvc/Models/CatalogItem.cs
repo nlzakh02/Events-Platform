ab4a8045b7dc8a3c1dd88021b980996866c96549
harveyx22 <52708955+harveyx22@users.noreply.github.com>
2020-07-27 21:15:37 -0700

    Added Catalog and CatalogItem

---
+++ WebMvc/Models/CatalogItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMvc.Models
+{
+    public class CatalogItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public decimal Price { get; set; }
+        public string PictureURL { get; set; }
+        public int EventOrganizerId { get; set; }
+        public int EventTypeId { get; set; }
+        public int EventAddressId { get; set; }
+        public int EventVenueId { get; set; }
+        public string EventOrganizer { get; set; }
+        public string EventType { get; set; }
+        public string EventAddress { get; set; }
+        public string EventVenue { get; set; }
+    }
+}
