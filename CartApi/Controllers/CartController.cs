0a2b7e098df9b79eab3e9f803192b69bbea7f90e
harveyx22 <52708955+harveyx22@users.noreply.github.com>
2020-08-07 22:17:41 -0700

    Added Cart Api and integrated with WebMvc

---
+++ CartApi/Controllers/CartController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using CartApi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CartApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class CartController : ControllerBase
+    {
+        private readonly ICartRepository _cartrepository;
+        public CartController(ICartRepository cartRepository)
+        {
+            _cartrepository = cartRepository;
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(Cart), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> Get(string id)
+        {
+            var basket = await _cartrepository.GetCartAsync(id);
+
+            return Ok(basket);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Cart), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> Post([FromBody]Cart value)
+        {
+            var basket = _cartrepository.UpdateCartAsync(value);
+            return Ok(basket);
+        }
+
+        [HttpDelete("{id}")]
+
+        public async void Delete(string id)
+        {
+            await _cartrepository.DeleteCartAsync(id);
+        }
+
+    }
+}
